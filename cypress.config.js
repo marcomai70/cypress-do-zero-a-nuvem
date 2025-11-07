00c700d4616d5cac17095d431c23e5d5f9524cc1
Marcos Maia <bateramarcomai@hotmail.com>
2025-11-07 13:52:24 -0300

    corrige o arq cupress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   projectId: '6vkr6w',
   video: false,
-  screenshotOnRunFailure: true
+  screenshotOnRunFailure: true,
   e2e: {
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
